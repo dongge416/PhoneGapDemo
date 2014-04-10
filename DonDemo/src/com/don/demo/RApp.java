f8523a8980d95c4d57075225bd1ede6818012bc3
dongge416 <dongge416@gmail.com>
2014-04-10 10:48:13 +0800

    Signed-off-by: dongge416 <dongge416@gmail.com>

---
+++ DonDemo/src/com/don/demo/RApp.java
@@ -0,0 +1,13 @@
+package com.don.demo;
+
+import android.app.Application;
+
+public class RApp extends Application{
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+	}
+	
+}
